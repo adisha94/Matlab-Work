07df868674368c0484edf7f5f7b66b9065dd8d07
Adish Betawar <abetawar@gmail.com>
2017-02-13 01:26:53 -0800

    stuff

--- Chem 1B/Functions/Range.m
+++ Chem 1B/Functions/Range.m
@@ -2,3 +2,5 @@ function[result] = Range(max, min)
 % finds the maximum and minimum values in the target dataset and calculates
 % the range for the set.
 result = max(target) - min(target);
+% try dealing with the condition where instad of supplying max and in
+% directly 
