dc32c53747a21c74d08c17a1d721367952f21072
Adish Betawar <abetawar@gmail.com>
2017-02-13 02:44:54 -0800

    edits

--- Chem 1B/Functions/Run_Analysis.m
+++ Chem 1B/Functions/Run_Analysis.m
@@ -2,5 +2,6 @@ function[marker] = Run_Analysis(run_Value)
 % Execute this function when running calculations for our datapoints
 disp('Funcion incomplete. Please input information for us to use for input')
 %%
-marker = 0;
+disp('Number of millimoles of oxalic acid')
+
 end
--- Chem 1B/exp16.m
+++ Chem 1B/exp16.m
@@ -4,6 +4,7 @@ Vol_Unknown = 15.00; % ml
 accepted = 0.2542;
 Storage
 NaOH_pellet_mass = input('What was the mass of the pellets?');
+
 %% Part 1: Standardizing NaOH solution with Oxalic Acid
 %disp('Expirement 16')
 % NaOH_grams = 4.00
@@ -12,7 +13,7 @@ final_NaOH_Vol = input('What was the final buret reading?');
 BuretRead(initial_Vol,final_NaOH_Vol)
 clear initial final
 
-%% Oxalic Acid
+% Oxalic Acid section
 Oxalic_Acid_Mass = input('Input the mass of the Oxalic acid sample that you acquired');
 disp(Oxalic_Acid_Mass)
 %Run_Analysis(Oxalic_Acid_Mass);
