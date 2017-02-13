d586ec0358ef4edfdf28450f0a2a074d04ed81d1
Adish Betawar <abetawar@gmail.com>
2017-02-13 01:23:34 -0800

    access important element info

--- Chem 1B/Functions/start_Run_Analysis.m
+++ Chem 1B/Functions/start_Run_Analysis.m
@@ -1,4 +1,5 @@
 function[marker] = start_Run_Analysis(run_Value)
 % Execute this function when running calculations for our datapoints
 disp('Funcion incomplete. Please input information for us to use for input')
+Storage
 end
