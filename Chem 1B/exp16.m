d3cbdb46f56f1b4b336feae381ee39863a55eb80
Adish Betawar <abetawar@gmail.com>
2017-02-13 02:14:22 -0800

    stuff

--- Chem 1B/exp16.m
+++ Chem 1B/exp16.m
@@ -8,4 +8,5 @@ Storage
 %% Part 1: Standardizing NaOH solution with Oxalic Acid
 %disp('Expirement 16')
 % NaOH_grams = 4.00
-NaOH_pellet_mass = 4.00; %g
+NaOH_pellet_mass = input('What was the mass of the pellets?');
+clear NaOH_pellet_mass % take this out when we finish the testing phase for this component
