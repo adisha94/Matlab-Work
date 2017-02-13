655bb112fc6ef6a04b04885464cdf42c7a86ce5d
Adish Betawar <abetawar@gmail.com>
2017-02-13 01:19:41 -0800

    new info

--- Chem 1B/Functions/start_Run_Analysis.m
+++ Chem 1B/Functions/start_Run_Analysis.m
@@ -1,2 +1,4 @@
 function[marker] = start_Run_Analysis(run_Value)
 % Execute this function when running calculations for our datapoints
+disp('Funcion incomplete. Please input information for us to use for input')
+end
