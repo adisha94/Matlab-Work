6b8d910a3d257280c3e2bdf7b7572ba6c931f8ff
Adish Betawar <abetawar@gmail.com>
2017-02-13 01:51:25 -0800

    tools

--- Chem 1B/Functions/Anlalysis_tools.m
+++ Chem 1B/Functions/Anlalysis_tools.m
@@ -8,3 +8,7 @@ function[result] = Deviation(ave, actual)
 result = ave - actual;
 end
 
+function[M] = Molarity(moles, Volume)
+%This function quickly helps to calculate the molarity for calculations
+M = moles / Volume;
+end
