66c89cff7e6dd39a9760b079f5ca39fefcd59e92
Adish Betawar <abetawar@gmail.com>
2017-03-02 18:39:25 -0800

    new expirement file and info

---
+++ Chem 1B/Expirement17.m
@@ -0,0 +1,8 @@
+ave_T = [355.5 345.5 337.5 327.5]; % numbers indicate average temperature
+ave_T = round(ave_T,2);
+t_elapsed = [66 95 248 493]; % time elapsed for reaction to reach equivalene point
+R = 8.314; % gas constant
+
+( log(2) / (1/355.5) - (1/327.5) ) * R;
+Ea = -ans
+A = ; % Arrehnius constant
