3a0c619cdab25d06b4c8b76c75e54f41fba879c2
Adish Betawar <abetawar@gmail.com>
2017-03-11 15:31:19 -0800

    made a mistake in comment

--- Chem 1B/Expirement17.m
+++ Chem 1B/Expirement17.m
@@ -3,4 +3,4 @@ ave_T = round(ave_T,2);
 t_elapsed = [66 95 248 493]; % time elapsed for reaction to reach equivalene point
 R = 8.314; % gas constant
 
-( log(2) / (1/355.5) - (1/327.5) ) * R; // calculations for one factor for stuff
+( log(2) / (1/355.5) - (1/327.5) ) * R; % calculations for one factor for stuff
