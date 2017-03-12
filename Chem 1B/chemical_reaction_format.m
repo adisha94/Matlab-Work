b99ff92f2d465bf64fdb74aa8c2de62416e6946c
Adish Betawar <abetawar@gmail.com>
2017-03-11 17:14:25 -0800

    new concept
    new program should reflect on the neeeds of the student when it comes to
    working with chemical reactions. THe idea should implement search and
    different datastructures so that we can make performing actions much
    more efficient

---
+++ Chem 1B/chemical_reaction_format.m
@@ -0,0 +1,6 @@
+% aA + bB = cC + dD
+% this is the format for a typical chemcial reaction
+
+
+Reactant = input('What is the chemical that is involved in the reaction?', 's')
+Product = input('What is the chemical that is involved in the reaction?', 's')
