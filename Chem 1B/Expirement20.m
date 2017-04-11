ac85ea5548bc810c70da27ba59c4e9f5d6132013
Adish Betawar <abetawar@gmail.com>
2017-04-10 17:18:55 -0700

    run analysis almost done

--- Chem 1B/Expirement20.m
+++ Chem 1B/Expirement20.m
@@ -1,4 +1,5 @@
-pH = input('Insert pH value');
+pH = input('Insert pH value'); % the calculations begin here
 H = round(10^(-pH),6) % for the H concentration
-OH = 1.00*10^(-14) / H; % for the OH concentration
-A = H
+OH = 1.00*10^(-14) / H
+% for the OH concentration
+A = H % same concentration as the H concentr ation
